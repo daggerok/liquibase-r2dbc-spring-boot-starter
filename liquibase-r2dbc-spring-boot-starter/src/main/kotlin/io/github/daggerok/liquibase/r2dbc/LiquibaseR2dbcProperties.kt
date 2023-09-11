feb5781adf6eb54b7a328d8d21553f59510b0591
Maksim Kostromin <maksim@beachfront.com>
2023-09-10 22:50:28 -0400

    Update project version to: 3.1.2-SNAPSHOT

--- liquibase-r2dbc-spring-boot-starter/src/main/kotlin/io/github/daggerok/liquibase/r2dbc/LiquibaseR2dbcProperties.kt
+++ liquibase-r2dbc-spring-boot-starter/src/main/kotlin/io/github/daggerok/liquibase/r2dbc/LiquibaseR2dbcProperties.kt
@@ -1,9 +1,7 @@
 package io.github.daggerok.liquibase.r2dbc
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
-@ConstructorBinding
 @ConfigurationProperties("spring.liquibase", ignoreUnknownFields = true)
 data class LiquibaseR2dbcProperties(
     @Suppress("UNUSED") val enabled: Boolean = true,
