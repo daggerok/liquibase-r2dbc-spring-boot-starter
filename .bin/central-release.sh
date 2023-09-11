84332f0a838a703c6c541d64b7a96d3269005541
Maksim Kostromin <maksim@beachfront.com>
2023-09-10 22:10:51 -0400

    Upgrade maven deploy release/snapshot bash scripts.

--- .bin/central-release.sh
+++ .bin/central-release.sh
@@ -4,9 +4,12 @@
 # Happy path release automation script
 #
 # Usage:
-#         GPG_PASSPHRASE=... bash .bin/central-release.sh
 #         bash .bin/central-release.sh -Dgpg.passphrase=...
+#         GPG_PASSPHRASE=... bash .bin/central-release.sh
 #         GPG_PASSPHRASE=... bash .bin/central-release.sh -DskipTests
+#         QUALIFIER=3 bash .bin/central-release.sh -Dgpg.passphrase=...
+#         QUALIFIER=3 GPG_PASSPHRASE=... bash .bin/central-release.sh
+#         QUALIFIER=3 GPG_PASSPHRASE=... bash .bin/central-release.sh -DskipTests
 #
 # Require:
 #         brew reinstall gnupg@2.2
@@ -51,34 +54,42 @@
 set -o pipefail
 set -e
 
-if [[ -n "$GPG_PASSPHRASE" ]] ; then
+if [[ -n "${GPG_PASSPHRASE}" ]] ; then
   echo "Detected GPG_PASSPHRASE environment variable"
-  export GPG_PASSPHRASE
 fi
 
+QUALIFIER=${QUALIFIER:-undefined}
+GIT_BRANCH=$(git branch --show-current)
 ARGS=${1:-"-Dgpg.passphrase=$GPG_PASSPHRASE"}
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
 cd "${ROOT_PROJECT_DIR}" && VERSION=$(bash mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
-echo "Deploying $VERSION version..."
+echo "Deploying ${VERSION} version..."
 
-if [[ "$VERSION" == *"-SNAPSHOT" ]] ; then
-  echo "Snapshot version was detected. Update $VERSION version to released, commit and push changes first..."
-  cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}
-  cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:commit initialize
-  cd "${ROOT_PROJECT_DIR}" && VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-  cd "${ROOT_PROJECT_DIR}" && git add . ; git commit -am "Release $VERSION version." ; git push origin master
+if [[ "${VERSION}" == *"-SNAPSHOT" ]] ; then
+  echo "Snapshot version was detected. Update ${VERSION} version to released, commit and push changes first..."
+  if [[ "${QUALIFIER}" == "undefined" ]] ; then
+    cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}
+  else
+    echo "Qualifier: ${QUALIFIER} was detected"
+    cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-${QUALIFIER}
+  fi
+
+  cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:commit
+  cd "${ROOT_PROJECT_DIR}" && VERSION=$(bash mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
+  cd "${ROOT_PROJECT_DIR}" && git add . ; git commit -am "Release ${VERSION} version." ; git push origin "${GIT_BRANCH}"
 fi
+
 echo "Deploying $VERSION release..."
-cd "${ROOT_PROJECT_DIR}" && ./mvnw -P central-release clean deploy ${ARGS}
+cd "${ROOT_PROJECT_DIR}" && ./mvnw -P central-release clean deploy "${ARGS}"
 
 echo "Creating $VERSION GitHub tag..."
-cd "${ROOT_PROJECT_DIR}" && git tag "$VERSION" ; git push origin "$VERSION"
+cd "${ROOT_PROJECT_DIR}" && git tag "${VERSION}" ; git push origin "${VERSION}"
 
 echo "Create new snapshot version for next development iteration..."
 cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT
 cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:commit
-cd "${ROOT_PROJECT_DIR}" && VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-cd "${ROOT_PROJECT_DIR}" && git add . ; git commit -am "Start next $VERSION development iteration." ; git push origin master
+cd "${ROOT_PROJECT_DIR}" && VERSION=$(bash mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
+cd "${ROOT_PROJECT_DIR}" && git add . ; git commit -am "Start next ${VERSION} development iteration." ; git push origin "${GIT_BRANCH}"
 
 echo "Released."
--- .bin/central-snapshot.sh
+++ .bin/central-snapshot.sh
@@ -51,22 +51,22 @@
 set -o pipefail
 set -e
 
-if [[ -n "$GPG_PASSPHRASE" ]] ; then
+if [[ -n "${GPG_PASSPHRASE}" ]] ; then
   echo "Detected GPG_PASSPHRASE environment variable"
 fi
 
-ARGS=${1:-"-Dgpg.passphrase=$GPG_PASSPHRASE"}
+ARGS=${1:-"-Dgpg.passphrase=${GPG_PASSPHRASE}"}
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
 cd "${ROOT_PROJECT_DIR}" && VERSION=$(bash mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
-echo "Deploying $VERSION version..."
+echo "Deploying ${VERSION} version..."
 
-if [[ "$VERSION" != *"-SNAPSHOT" ]] ; then
+if [[ "${VERSION}" != *"-SNAPSHOT" ]] ; then
   echo "Snapshot version was not found. Exiting..."
   exit 1
 fi
 
-echo "Deploying $VERSION..."
-cd "${ROOT_PROJECT_DIR}" && bash mvnw -P central-release clean deploy ${ARGS}
+echo "Deploying ${VERSION}..."
+cd "${ROOT_PROJECT_DIR}" && bash mvnw -P central-release clean deploy "${ARGS}"
 
 echo "Snapshot released."
